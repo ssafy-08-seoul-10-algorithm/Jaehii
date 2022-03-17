d71dfba101afc74a49169b2a79d9c15ea138de9d
JaeheeChoi <60592289+jaehii@users.noreply.github.com>
2022-03-17 16:40:07 +0900

    Add files via upload

---
+++ 1026.py
@@ -0,0 +1,13 @@
+x = input()
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+sum = 0
+
+arr2.sort(reverse=True)    # B배열은 큰것부터
+arr1.sort()    # A배열은 작은것부터
+
+for i, j in zip(arr1, arr2):
+    k = i * j
+    sum += k
+
+print(sum)
